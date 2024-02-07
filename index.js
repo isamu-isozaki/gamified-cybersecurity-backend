0f6873deb988017173306155593695440f61ba26
chi-p-nguyen <phuongchi2782001@gmail.com>
2024-02-07 10:54:10 -0500

    Add socket code

--- index.js
+++ index.js
@@ -4,28 +4,32 @@ require('env-smart').load()
 
 const { APP_PORT } = require('@/config')
 const { sshConnect } = require('@/ssh')
+const socketManager = require('@/ssh/socketModule')
+const cors = require('cors');
+const http = require('http');
 
 async function startApp () {
   startExpressApp()
-  sshConnect()
+  //sshConnect()
 }
 
 async function startExpressApp () {
   const express = require('express')
   const app = express()
-  const { landingPageCors } = require('@/middlewares/cors')
-  app.use(landingPageCors)
-
-  const bodyParser = require('body-parser')
-  app.use(bodyParser.json({
-    limit: '50mb'
-  }))
+  app.use(cors({
+    origin: '*',
+    credentials: true
+    }));
+  app.use(express.json());
 
   const cookieParser = require('cookie-parser')
   app.use(cookieParser())
 
-  const responsesMiddleware = require('@/middlewares/responses')
-  app.use(responsesMiddleware)
+  const responsesMiddleware = require('@/middlewares/responses');
+  app.use(responsesMiddleware);
+
+  const server = http.createServer(app);
+  socketManager(server);
 
   const router = require('@/app/router')
   app.use('/v1', router)
@@ -33,17 +37,21 @@ async function startExpressApp () {
   app.use((err, req, res, next) => {
     console.log(err)
     res.serverError()
-  })
+  });
 
-  app.use((req, res) => {
+  /* app.use((req, res) => {
     const fullUrl = req.protocol + '://' + req.get('host') + req.originalUrl;
     console.log({fullUrl})
     res.notFound()
-  })
+  }) */
 
-  app.listen(APP_PORT, () => {
+  app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/public/index.html');
+});
+
+  server.listen(APP_PORT, () => {
     console.log(`Server listening on port ${APP_PORT}`)
-  })
+  });
 }
 
 startApp()
---
+++ src/ssh/socketModule.js
@@ -0,0 +1,29 @@
+const { sshConnect, doCommand } = require('@/ssh');
+const socketIO = require('socket.io');
+
+module.exports = (server) => {
+    sshConnect();
+
+    const io = socketIO(server, {
+        cors: {
+            origin: 'http://localhost:3000',
+            methods: ['GET', 'POST'],
+        }
+    });
+
+    io.on('connection', (socket) => {
+        console.log('a user connected');
+
+        socket.on('command', async (command) => {
+            console.log('command', command);
+            const result = await doCommand(command);
+            console.log('result', result);
+            socket.emit('commandResult', result);
+        });
+
+        socket.on('disconnect', () => {
+            console.log('User disconnected:', socket.id);
+            socket.removeAllListeners();
+        });
+    });
+}
